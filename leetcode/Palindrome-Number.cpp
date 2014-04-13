875e920c7c640dd5a3742f1e88408032906382df
Jesse Chen <jaleel.chen@gmail.com>
2014-04-13 12:09:59 -0600

    Update: xxx

--- leetcode/Palindrome-Number.cpp
+++ leetcode/Palindrome-Number.cpp
@@ -1,3 +1,5 @@
+// From leetcode
+// http://leetcode.com/2012/01/palindrome-number.html
 class Solution {
 public:
     bool isPalindrome(int x) {
@@ -12,6 +14,26 @@ public:
     
         if(!isPalindrome(x/10, y) || x%10 != y%10) return false;
     
+        y /= 10;
+        return true;
+    }
+};
+
+// alternative
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        return isPalindrome(x, x);
+    }
+    
+    bool isPalindrome(int x, int& y)
+    {
+        if(x < 0) return false;
+        if(x == 0) return true;
+        
+        if(!isPalindrome(x/10, y) || (x % 10 != y % 10)) 
+            return false;
+            
         y /= 10;
         return true;
     }

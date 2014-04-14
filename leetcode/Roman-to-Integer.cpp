199d047221e79a8f8186ac8b5c12c391f66f4587
Jesse Chen <jaleel.chen@gmail.com>
2014-04-14 11:21:16 -0600

    Update: Roman to integer

--- leetcode/Roman-to-Integer.cpp
+++ leetcode/Roman-to-Integer.cpp
@@ -15,3 +15,32 @@ public:
             return um[s.substr(0, 1)] + romanToInt(s.substr(1));
     }
 };
+
+
+// alternative slightly better, no recursive
+class Solution {
+public:
+
+   int romanToInt(string s) {
+       // Note: The Solution object is instantiated only once and is reused by each test case.
+       static unordered_map<char, int> um({{'M', 1000}, {'D', 500}, {'C', 100}, {'L', 50}, 
+                                             {'X', 10}, {'V', 5}, {'I', 1}});
+        // from last character to first character
+        int sz = s.size();
+        int sum = 0;
+        for(int i=sz-1; i>=0; --i)
+        {
+            if(i-1 >= 0 && um[s[i-1]] < um[s[i]]) 
+            {
+                sum = sum + um[s[i]] - um[s[i-1]];
+                --i;
+            }
+            else
+            {
+                sum += um[s[i]];
+            }
+        }//for
+        
+        return sum;
+   }
+};
